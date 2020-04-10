2f5e1c72791db447d5a58c24732870f082cf4200
Dawn Yue <1179348863@qq.com>
2020-04-10 14:44:09 +0800

    练习3

--- Hough/Hough.cpp
+++ Hough/Hough.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 using namespace cv;
 
-//练习2
+//练习3
 int main()
 {
 	cv::Mat mat_gray;
@@ -24,28 +24,16 @@ int main()
 	cv::Canny(mat_binary, mat_canny, 50, 125, 3);
 
 	// detect line
-	std::vector<cv::Vec2f> lines;
-	cv::HoughLines(mat_canny, lines, 1, CV_PI / 180, 100);
-
-	cv::Mat lineMat;
-	cv::HoughLines(mat_canny, lineMat, 1, CV_PI / 180, 100);
-	//float r = lines[0][0];
-	//float t = lines[0][1];
+	std::vector<cv::Vec4i> lines;
+	cv::HoughLinesP(mat_canny, lines, 1, CV_PI / 180, 35, 10,10);
 
 	// draw line
-	std::vector<cv::Vec2f>::iterator it = lines.begin();
-	for (; it != lines.end(); ++it)
+	for (size_t i = 0; i < lines.size(); i++)
 	{
-		float rho = (*it)[0], theta = (*it)[1];
-		cv::Point pt1, pt2;
-		double a = cos(theta), b = sin(theta);
-		double x0 = a * rho, y0 = b * rho;		
-		pt1.x = cv::saturate_cast<int>(x0 + 1000 * (-b));
-		pt1.y = cv::saturate_cast<int>(y0 + 1000 * (a));
-		pt2.x = cv::saturate_cast<int>(x0 - 1000 * (-b));
-		pt2.y = cv::saturate_cast<int>(y0 - 1000 * (a));
-		cv::line(srcImage, pt1, pt2, cv::Scalar(0, 0, 255),1, CV_AA);
-	}       
+		cv::Vec4i linex = lines[i];
+		line(srcImage, cv::Point(linex[0], linex[1]), cv::Point(linex[2], linex[3]), cv::Scalar(0, 0, 255), 1, CV_AA);
+	}  
+
 
 	cv::imshow("canny", mat_canny);
 	cv::imshow("mat", srcImage);
