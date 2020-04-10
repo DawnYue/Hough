03abae057c8e60ee9fccf515c4cea0ec5d10039f
Dawn Yue <1179348863@qq.com>
2020-04-10 12:50:34 +0800

    课前准备

--- Hough/Hough.cpp
+++ Hough/Hough.cpp
@@ -16,6 +16,31 @@ int main()
 	img_path = "../pictures/000177.png";
 	mat_color = cv::imread("E:\\4.png", 1);
 	mat_gray = cv::imread("E:\\4.png", 0);
+	//【3】转为灰度图并进行图像平滑
+	Mat srcImage = imread("E:\\4.png");  
+	Mat midImage, dstImage;
+	cvtColor(srcImage, midImage, COLOR_BGR2GRAY);//转化边缘检测后的图为灰度图
+	GaussianBlur(midImage, midImage, Size(9, 9), 2, 2);
+	//【4】进行霍夫圆变换
+	vector<Vec3f> circles;
+	HoughCircles(midImage, circles, HOUGH_GRADIENT, 1.5, 10, 200, 100, 0, 0);
+	//【5】依次在图中绘制出圆
+	for (size_t i = 0; i < circles.size(); i++)
+	{
+		//参数定义
+		Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
+		int radius = cvRound(circles[i][2]);
+		//绘制圆心
+		circle(srcImage, center, 3, Scalar(0, 255, 0), -1, 8, 0);
+		//绘制圆轮廓
+		circle(srcImage, center, radius, Scalar(155, 50, 255), 3, 8, 0);
+	}
+
+	//【6】显示效果图  
+	imshow("【效果图】", srcImage);
+
+
+
 
 	//形态学闭运算
 	cv::Mat elementRect;
